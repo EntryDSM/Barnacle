3361e51dfb535d9c2cf607df52b2d6a3a338de18
rlaisqls <rlaisqls@gmail.com>
2023-05-29 01:37:08 +0900

    update: dotenv

--- src/config/properties.js
+++ src/config/properties.js
@@ -1,19 +1,12 @@
-const {
-    MONGO_DB_URI,
-    AWS_REGION,
-    AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY,
-    AWS_S3_URL,
-    AWS_S3_BUCKET,
-    PDF_FOLDER
-} = process.env;
+const dotenv = require('dotenv');
+const env = dotenv.config().parsed;
 
-exports = {
-    dbUri: MONGO_DB_URI,
-    region: AWS_REGION,
-    access: AWS_ACCESS_KEY_ID,
-    secret: AWS_SECRET_ACCESS_KEY,
-    s3Url: AWS_S3_URL,
-    bucket: AWS_S3_BUCKET,
-    pdfFolder: PDF_FOLDER
+module.exports = {
+    dbUri: env.MONGO_DB_URI,
+    region: env.AWS_REGION,
+    access: env.AWS_ACCESS_KEY_ID,
+    secret: env.AWS_SECRET_ACCESS_KEY,
+    s3Url: env.AWS_S3_URL,
+    bucket: env.AWS_S3_BUCKET,
+    pdfFolder: env.PDF_FOLDER
 }
