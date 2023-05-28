a8697c6d98b36603056ffc0beef9b8570072f9c0
rlaisqls <rlaisqls@gmail.com>
2023-05-29 01:37:22 +0900

    update: 주석 정리

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
-var express = require("express");
-//var dbConnect = require('./database/mongoDB')
-var router = require('./web/router')
+const express = require("express");
 const mongoDB = require('./database/mongoDB')
+const router = require('./web/router')
 
 mongoDB.dbConnect()
 
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cors());
-
-//Route Prefixes
 app.use("/", router);
 app.listen(8080)
 
