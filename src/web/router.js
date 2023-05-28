302e408c616666835720c77c711e1caf3c1ea583
rlaisqls <rlaisqls@gmail.com>
2023-05-29 01:39:32 +0900

    update: libraryDocument 저장

--- src/web/router.js
+++ src/web/router.js
@@ -1,18 +1,27 @@
-const { processTemplateByDocuments } = require('../file/fileProcessor')
+const { processTemplateByDocuments } = require('../file/teplateProcessor')
 const { generatePdf } = require('../file/pdfGenerator')
-const { getDocumentsByGrade } = require('../database/mongoDB')
+const s3 = require('../file/s3Adapter')
+const document = require('../database/document')
+const libraryDocument = require('../database/libraryDocument')
 const express = require("express");
 
 var router = express.Router();
 
 router.post("/", async (req, res) => { 
-  const documents = await getDocumentsByGrade(req.query.grade)
+
+  const grade = req.query.grade
+  const documents = await document.getByGrade(grade)
   const templates = await processTemplateByDocuments(documents)
   
+  const pdf = await generatePdf(templates)
+  const year = req.query.year
+  const fileName = String(year) + '_' + String(grade) + '_' + new Date()
+  const key = s3.savePdfFile(fileName, pdf)
+
+  libraryDocument.create(year, grade, key, documents)
+  
   res.setHeader('Content-Type', 'application/pdf');
-  res.send(
-    await generatePdf(templates)
-  )
+  res.send(pdf)
 });
 
 module.exports = router;
