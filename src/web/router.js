b5dc5f47e60377c4b4ba72a6f009b983975804b7
rlaisqls <rlaisqls@gmail.com>
2023-05-29 16:41:44 +0900

    refactor: method 분리

--- src/web/router.js
+++ src/web/router.js
@@ -1,5 +1,6 @@
 const { processTemplateByDocuments } = require('../file/teplateProcessor')
 const { generatePdf } = require('../file/pdfGenerator')
+const properties = require('../config/properties')
 const s3 = require('../file/s3Adapter')
 const document = require('../database/document')
 const libraryDocument = require('../database/libraryDocument')
@@ -9,12 +10,13 @@ var router = express.Router();
 
 router.post("/", async (req, res) => { 
 
+  checkSecret(req, res)
   const year = req.query.year
   const grade = req.query.grade
 
   const documents = await document.getByGrade(grade)
-  const templates = await processTemplateByDocuments(documents)
-  const pdf = await generatePdf(templates)
+
+  const pdf = await generatePdfByDocuments(documents)
 
   const key = await s3.savePdfFile(getFilename(year, grade), pdf)
   libraryDocument.create(year, grade, key, documents)
@@ -23,9 +25,20 @@ router.post("/", async (req, res) => {
   res.send(pdf)
 });
 
+async function generatePdfByDocuments(documents) {
+  const templates = await processTemplateByDocuments(documents)
+  return await generatePdf(templates)
+}
+
 function getFilename(year, grade) {
   const date = new Date()
   return String(year) + '_' + String(grade) + '_' + date.getFullYear() + date.getMonth() + date.getDay() + ".pdf"
+}
+
+function checkSecret(req, res) {
+  if (req.query.secret != properties.secret) {
+    res.status(403).send()
+  }
 }
 
 module.exports = router;
