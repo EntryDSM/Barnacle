4f62dafa2788af3add8ddca405afd6f836dc9d7d
rlaisqls <rlaisqls@gmail.com>
2023-05-29 16:23:07 +0900

    refactor: filename 함수 분리

--- src/web/router.js
+++ src/web/router.js
@@ -9,19 +9,23 @@ var router = express.Router();
 
 router.post("/", async (req, res) => { 
 
+  const year = req.query.year
   const grade = req.query.grade
+
   const documents = await document.getByGrade(grade)
   const templates = await processTemplateByDocuments(documents)
-  
   const pdf = await generatePdf(templates)
-  const year = req.query.year
-  const fileName = String(year) + '_' + String(grade) + '_' + new Date()
-  const key = s3.savePdfFile(fileName, pdf)
 
+  const key = await s3.savePdfFile(getFilename(year, grade), pdf)
   libraryDocument.create(year, grade, key, documents)
   
   res.setHeader('Content-Type', 'application/pdf');
   res.send(pdf)
 });
+
+function getFilename(year, grade) {
+  const date = new Date()
+  return String(year) + '_' + String(grade) + '_' + date.getFullYear() + date.getMonth() + date.getDay() + ".pdf"
+}
 
 module.exports = router;
