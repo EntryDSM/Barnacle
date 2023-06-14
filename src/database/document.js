65d4576336c8c24508cb3ab1d202e2e57f0bf1df
rlaisqls <rlaisqls@gmail.com>
2023-06-14 22:12:45 +0900

    feat: preview api 및 qr 추가

--- src/database/document.js
+++ src/database/document.js
@@ -1,8 +1,21 @@
 const { client } = require('./mongoDB')
 
-exports.getByGrade = async (grade) => {
+exports.getByGradeAndYear = async (grade, year) => {
 	return await client.db('repo')
 		.collection('documents')
-		.find({ 'writer.grade' : Number(grade) })
+		.find({
+			'writer.grade' : Number(grade),
+			'year' : Number(year)
+		})
 		.toArray()
+		.then((documents) => 
+			documents.map((it) => {
+				const writer = it.writer
+				if ( writer.grade == '1' ) it.writer.department = '공통과정'
+				else if ( writer.classNum == '1' || writer.classNum == '2' ) it.writer.department = '소프트웨어개발과'
+				else if ( writer.classNum == '3' ) it.writer.department = '임베디드소프트웨어과'
+				else if ( writer.classNum == '4' ) it.writer.department = '정보보안과'
+				return it
+			})
+		)
 }
--- src/web/router.js
+++ src/web/router.js
@@ -1,50 +1,100 @@
 const { processTemplateByDocuments } = require('../file/teplateProcessor')
-const { generatePdf } = require('../file/pdfGenerator')
+const { generatePdf, generatePdfByUrl } = require('../file/pdfGenerator')
 const properties = require('../config/properties')
 const s3 = require('../file/s3Adapter')
 const document = require('../database/document')
 const libraryDocument = require('../database/libraryDocument')
 const express = require("express");
-
 var router = express.Router();
 
-router.post("/library", async (req, res) => { 
+router.post("/library", async (req, res) => {
+    const {year, grade, documents, pdf} = await checkParamAndGetPdf(req, res);
 
-  checkSecret(req, res)
-  
-  if (!req.query['year'] || !req.query['grade']) {
-    res.status(400).send()
-  }
-  const year = req.query.year
-  const grade = req.query.grade
+    const key = await s3.savePdfFile(getFilename(year, grade), pdf)
+    libraryDocument.create(year, grade, key, documents)
 
-  const documents = await document.getByGrade(grade)
-  console.log("create pdf file to " + year + ", " + grade + "'s documents")
-  console.log(documents)
-
-  const pdf = await generatePdfByDocuments(documents)
-
-  const key = await s3.savePdfFile(getFilename(year, grade), pdf)
-  libraryDocument.create(year, grade, key, documents)
-  
-  res.setHeader('Content-Type', 'application/pdf');
-  res.send(pdf)
+    res.setHeader('Content-Type', 'application/pdf');
+    res.send(pdf)
 });
-
-async function generatePdfByDocuments(documents) {
-  const templates = await processTemplateByDocuments(documents)
-  return await generatePdf(templates)
-}
 
 function getFilename(year, grade) {
   const date = new Date()
   return String(year) + '_' + String(grade) + '_' + date.getFullYear() + date.getMonth() + date.getDay() + ".pdf"
 }
 
+router.post("/library/preview", async (req, res) => {
+    const {year, grade, documents, pdf} = await checkParamAndGetPdf(req, res);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.send(pdf)
+});
+
+async function checkParamAndGetPdf(req, res) {
+    checkSecret(req, res)
+
+    if (!req.query['year'] || !req.query['grade']) {
+        res.status(400).send()
+    }
+    const year = req.query.year
+    const grade = req.query.grade
+
+    const documents = await document.getByGradeAndYear(grade, year)
+    console.log("create pdf file to " + year + ", " + grade + "'s documents")
+
+    const pdf = await generatePdfByDocuments(documents)
+    return {year, grade, documents, pdf};
+}
+
+async function generatePdfByDocuments(documents) {
+    const templates = await processTemplateByDocuments(documents)
+    return await generatePdf(templates)
+}
+
 function checkSecret(req, res) {
-  if (req.query.secret != properties.secret) {
-    res.status(403).send()
-  }
+    if (req.query.secret != properties.secret) {
+        res.status(403).send()
+    }
 }
 
 module.exports = router;
+
+/*
+
+2022 repo migration
+router.post("/document/test", async (req, res) => {
+
+  const datas = data
+    .filter(s => s.gcn.startsWith('2'))
+
+  var page = 0
+  const indexes = []
+  datas.sort(function(a, b) {
+    return parseInt(a.gcn) - parseInt(b.gcn);
+})
+
+  merger.reset()
+  for (const s of datas) {
+    const pdf = await generatePdfByUrl('https://admin.dsm-repo.com/view/' + s.student_id)
+    await merger.add(pdf)
+    const curPage = page
+    page += await getNumPages(pdf)
+    console.log(page)
+    indexes.push({
+      name: s.name,
+      major: s.major_tag,
+      studentNumber: s.gcn,
+      page: curPage
+    })
+  }
+
+  await merger.save('2022_2_2023610.pdf');
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(indexes)
+});
+
+const getNumPages = async (file) => {
+  return pdfcounter(file).then(function(data) {
+    return data.numpages
+  });
+}
+*/
